f3ee64d14abf8c92fbcd2f10e98c58ec099395f0
Fox-in-the-forest <gologrin69@mail.ru>
2023-03-01 23:00:20 +0300

    changes made 01

--- lesson_1/IfElseStatementTheme.java
+++ lesson_1/IfElseStatementTheme.java
@@ -2,28 +2,28 @@ public class IfElseStatementTheme {
     public static void main(String[] args) {
         System.out.println("1. Перевод псевдокода на язык Java");
         int age = 21;
-        boolean male = true;
-        double growth = 1.85;
-        char firstLetterOfName = "Iren".charAt(0);
         if (age > 20) {
             System.out.println("Вход возможен.");
         } else {
             System.out.println("Запрещено. Вам нет 20 лет.");
         }
+        boolean male = true;
         if (!male) {
             System.out.println("Женщинам сегодня скидка, поздравляю!");
         } else {
             System.out.println("Для мужчин акции в следующем месяце.");
         }
-        if (growth < 1.80) {
+        double height = 1.85;
+        if (height < 1.80) {
             System.out.println("Вы не приняты в баскетбольную команду.");
         } else {
             System.out.println("Поздравляю, Вам присвоен номер 7.");
         }
-        if (firstLetterOfName == 'М') {
-            System.out.println("Ваше имя начинается на " + firstLetterOfName);
-        } else if (firstLetterOfName == 'I') {
-            System.out.println("Ваше имя начинается на " + firstLetterOfName);
+        char capitalLetter = "Iren".charAt(0);
+        if (capitalLetter == 'М') {
+            System.out.println("Ваше имя начинается на " + capitalLetter);
+        } else if (capitalLetter == 'I') {
+            System.out.println("Ваше имя начинается на " + capitalLetter);
         } else {
             System.out.println("Символ не распознан");
         }
@@ -32,48 +32,52 @@ public class IfElseStatementTheme {
         int number1 = 13;
         int number2 = 51;
         if (number2 < number1) {
-            System.out.println(number2 + " и " + number1 + ". Минимальное число = " + number2 +
-                ". Максимальное число = " + number1);
+            System.out.println(number2 + " и " + number1 + ". Минимальное число = " + number2
+                    + ". Максимальное число = " + number1);
         } else if (number2 > number1) {
-            System.out.println(number2 + " и " + number1 + ". Минимальное число = " + number1 +
-                ". Максимальное число = " + number2);
+            System.out.println(number2 + " и " + number1 + ". Минимальное число = "
+                    + number1 + ". Максимальное число = " + number2);
         } else {
             System.out.println("Числа " + number2 + " и " + number1 + " равны.");
         }
 
         System.out.println("\n3. Проверка числа");
-        int verifiedNumber = 13;
-        System.out.println(verifiedNumber);
-        if (verifiedNumber != 0) {
-            if (verifiedNumber % 2 == 0) {
+        int srcNumber = -7;
+        System.out.println(srcNumber);
+        if (srcNumber != 0) {
+            if (srcNumber % 2 == 0) {
                 System.out.println("Число четное");
             } else {
                 System.out.println("Число нечетное");
             }
-            if (verifiedNumber > 0) {
+            if (srcNumber > 0) {
                 System.out.println("Число положительное");
-            } else System.out.println("Число отрицательное");
+            } else {
+                System.out.println("Число отрицательное");
+            }
         } else {
             System.out.println("Проверки не проводятся");
         }
 
         System.out.println("\n4. Поиск одинаковых цифр в числах");
-        int number3 = 129;
-        int number4 = 329;
-        int number3Ones = number3 % 10;
-        int number3Tens = number3 % 100 / 10;
+        int number3 = 123;
         int number3Hundreds = number3 / 100;
-        int number4Ones = number4 % 10;
-        int number4Tens = number4 % 100 / 10;
+        int number3Tens = number3 % 100 / 10;
+        int number3Ones = number3 % 10;
+        int number4 = 223;
         int number4Hundreds = number4 / 100;
-        if (number3Hundreds == number4Hundreds | number3Tens == number4Tens
-            | number3Ones == number4Ones) {
+        int number4Tens = number4 % 100 / 10;
+        int number4Ones = number4 % 10;
+        if (number3Hundreds == number4Hundreds || number3Tens == number4Tens || number3Ones 
+                == number4Ones) {
             System.out.println("Исходные числа: " + number3 + " и " + number4);
             if (number3Hundreds == number4Hundreds) {
                 System.out.println("Одинаковая цифра " + number3Hundreds + " 1ый разряд");
-            } else if (number3Tens == number4Tens) {
+            }
+            if (number3Tens == number4Tens) {
                 System.out.println("Одинаковая цифра " + number3Tens + " 2ой разряд");
-            } else if (number3Ones == number4Ones) {
+            }
+            if (number3Ones == number4Ones) {
                 System.out.println("Одинаковая цифра " + number3Ones + " 3ий разряд");
             }
         } else {
@@ -81,17 +85,17 @@ public class IfElseStatementTheme {
         }
 
         System.out.println("\n5. Определение символа по его коду");
-        char unicodeCharacter = '\u0032';
-        if (unicodeCharacter >= 'a' && unicodeCharacter <= 'z') {
+        char someChar = '\u0032';
+        if (someChar >= 'a' && someChar <= 'z') {
             System.out.printf("маленькая буква");
-        } else if (unicodeCharacter >= 'A' && unicodeCharacter <= 'Z') {
+        } else if (someChar >= 'A' && someChar <= 'Z') {
             System.out.printf("большая буква");
-        } else if (unicodeCharacter >= '\u0030' && unicodeCharacter <= '\u0039') {
+        } else if (someChar >= '1' && someChar <= '9') {
             System.out.printf("цифра");
         } else {
             System.out.printf("не буква и не число");
         }
-        System.out.printf(" " + unicodeCharacter + "\n");
+        System.out.println(" " + someChar + "\n");
 
         System.out.println("\n6. Подсчет суммы вклада и начисленных банком %");
         int contribution = 300000;
@@ -155,20 +159,21 @@ public class IfElseStatementTheme {
         int numberOfHundreds = requiredAmount / 100;
         int numberOfTens = requiredAmount / 10 % 10;
         int numbersOfOnes = requiredAmount % 10;
-        System.out.println("100$ - " + amountHundreds + " шт." + " 10$ - " + amountTens
-            + "шт." + " 1$ - " + amountOnes + " шт. Итого : " + allMoneyATM + "$"); // наглядно
+        System.out.println("100$ - " + amountHundreds + " шт." + " 10$ - " + amountTens + "шт."
+                + " 1$ - " + amountOnes + " шт. Итого : " + allMoneyATM + "$"); // наглядно
         if (allMoneyATM < requiredAmount) {
             System.out.println("Операция отклонена. Недостаточно средств.");
         } else {
-            if (amountHundreds >= numberOfHundreds && amountTens >= numberOfTens
-                && amountOnes>=numbersOfOnes) {
-                System.out.println("100$ - " + numberOfHundreds + " шт." + " 10$ - " + numberOfTens
-                    + "шт." + " 1$ - " + numbersOfOnes + " шт. Итого : " + requiredAmount + "$");
+            if (amountHundreds >= numberOfHundreds && amountTens >= numberOfTens && amountOnes
+                    >= numbersOfOnes) {
+                System.out.println("100$ - " + numberOfHundreds + " шт." + " 10$ - " +
+                        numberOfTens + "шт." + " 1$ - " + numbersOfOnes + " шт. Итого : " +
+                        requiredAmount + "$");
             }
-            if(numberOfTens > amountTens) {
-                amountOnes =  ((numberOfTens - amountTens) * 10) + numbersOfOnes;
-                System.out.println("100$ - " + numberOfHundreds + " шт." + " 10$ - " + amountTens
-                    + "шт." + " 1$ - " + amountOnes + " шт. Итого : " + requiredAmount + "$");
+            if (numberOfTens > amountTens) {
+                amountOnes = ((numberOfTens - amountTens) * 10) + numbersOfOnes;
+                System.out.println("100$ - " + numberOfHundreds + " шт." + " 10$ - " + amountTens +
+                        "шт." + " 1$ - " + amountOnes + " шт. Итого : " + requiredAmount + "$");
             }
         }
     }
